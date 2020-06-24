b22ad25cb5e529cbc22df4c83fcc4667adb945a7
Kristie Huang <kristiehuang@fb.com>
2020-06-24 09:35:27 -0700

    Initial Commit

---
+++ Blockbuster-Mobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blockbuster-Mobile
+//
+//  Created by Kristie Huang on 6/24/20.
+//  Copyright © 2020 Kristie Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Blockbuster-Mobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blockbuster-Mobile
+//
+//  Created by Kristie Huang on 6/24/20.
+//  Copyright © 2020 Kristie Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
