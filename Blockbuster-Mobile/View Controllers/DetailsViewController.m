e94ad2a99272908fc21c7f4209e551b259ec18bf
Kristie Huang <kristiehuang@fb.com>
2020-06-26 12:29:32 -0700

    Add NSLog

--- Blockbuster-Mobile/View Controllers/DetailsViewController.m
+++ Blockbuster-Mobile/View Controllers/DetailsViewController.m
@@ -66,6 +66,7 @@
 - (NSDictionary*)getVideos {
     //fetch data
     NSNumber *movie_id = self.movie[@"id"]; //wrong type
+    NSLog(@"What it dooo");
     //fetch data from video endpoint using /movie/movie_id/videos
     //videos[key[
     return self.movie[@"videos"];
