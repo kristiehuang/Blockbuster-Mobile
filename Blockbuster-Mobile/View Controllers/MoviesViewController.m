3cc7b5fe8406f878833a497c75dab4c5dcbfe6df
Kristie Huang <kristiehuang@fb.com>
2020-07-01 16:48:56 -0700

    Small changes to the way UI updates are made in main thread

--- Blockbuster-Mobile/View Controllers/MoviesViewController.m
+++ Blockbuster-Mobile/View Controllers/MoviesViewController.m
@@ -56,34 +56,36 @@
     [manager fetchNowPlaying:^(NSArray * _Nonnull movies, NSError * _Nonnull error) {
         
         if (error != nil) {
-                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Oops!" message:[error localizedDescription] preferredStyle:(UIAlertControllerStyleAlert)];
-                UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-                }];
-                UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Try again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                    [self fetchMovies];
-                }];
-                [alert addAction:cancelAction];
-                [alert addAction:okAction];
-                [self presentViewController:alert animated:YES completion:^{
-                }];
-           }
-           else {
-               self.movies = [[NSMutableArray alloc] init];
-
-               for (NSDictionary *dict in movies) {
-                   Movie *m = [[Movie alloc] initWithDictionary:dict];
-                   [self.movies addObject:m];
-               }
-               self.filteredMovies = self.movies;
-
-               [self.tableView performSelectorOnMainThread:NSSelectorFromString(@"reloadData") withObject:nil waitUntilDone:YES];
-//               [self.tableView reloadData];
-
-           }
-
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Oops!" message:[error localizedDescription] preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            }];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Try again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [self fetchMovies];
+            }];
+            [alert addAction:cancelAction];
+            [alert addAction:okAction];
+            [self presentViewController:alert animated:YES completion:^{
+            }];
+        }
+        else {
+            self.movies = [[NSMutableArray alloc] init];
+            
+            for (NSDictionary *dict in movies) {
+                Movie *m = [[Movie alloc] initWithDictionary:dict];
+                [self.movies addObject:m];
+            }
+            self.filteredMovies = self.movies;
+            
+            //               [self.tableView performSelectorOnMainThread:NSSelectorFromString(@"reloadData") withObject:nil waitUntilDone:YES];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.tableView reloadData];
+                [self.refreshControl endRefreshing];
+            });
+            
+        }
+        
     }];
-    [self.refreshControl endRefreshing];
- 
+    
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -101,8 +103,7 @@
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
     if (searchText.length != 0) {
         NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(Movie* evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
-            NSString *searched = [[evaluatedObject.title stringByAppendingString:
-                                   [NSString stringWithFormat:@" %@", evaluatedObject.synopsis]] lowercaseString];
+            NSString *searched = [[NSString stringWithFormat:@"%@ %@",evaluatedObject.title,  evaluatedObject.synopsis] lowercaseString];
             return [searched containsString:[searchText lowercaseString]];
         }];
         self.filteredMovies = [NSMutableArray arrayWithArray:[self.movies filteredArrayUsingPredicate:predicate]];
@@ -111,7 +112,7 @@
         self.filteredMovies = self.movies;
     }
     [self.tableView reloadData];
-
+    
 }
 
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
@@ -124,7 +125,7 @@
     [self.searchBar resignFirstResponder];
     [self searchBar:self.searchBar textDidChange:@""];
     [self.tableView reloadData];
-
+    
 }
 
 @end
